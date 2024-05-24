cf3d574b0a896a112b6d71d1e7db65b74d93f1c0
intafy <bruno89@mail.ru>
2024-05-24 06:41:54 +0400

    First commit. I have created a webview app that fetches an image from the phone's local storage and uploads it to the web app.

--- app/src/main/java/com/intafy/webviewfotochooser/MainActivity.java
+++ app/src/main/java/com/intafy/webviewfotochooser/MainActivity.java
@@ -3,24 +3,17 @@ package com.intafy.webviewfotochooser;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private ValueCallback<Uri[]> mFilePathCallBack;
-    private ValueCallback<Uri> mUploadMsg;
-    private Uri uri;
     private final int REQ_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
